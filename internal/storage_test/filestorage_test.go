c746bea4861b068aeeb118c0d78dc5979a93292d
Amos Jun-yeung Ng <me@amos.ng>
2025-09-03 22:15:06 +0700

    Test node deletion

--- internal/storage_test/filestorage_test.go
+++ internal/storage_test/filestorage_test.go
@@ -74,6 +74,31 @@ func GenericStorage_TestUpdateNode(t *testing.T, storage Storage, node models.No
 	assert.Equal(t, readNode.Slug(), newTestSlug)
 }
 
+func GenericStorage_TestDeleteNode(t *testing.T, storage Storage, node models.Node) {
+	node.SetTitle(testTitle)
+	node.SetContent(testDescription)
+	node.SetSlug(testSlug)
+
+	// set up new node
+	err := storage.WriteNode(node)
+	assert.NoError(t, err)
+
+	// first, verify that the node exists and we can read it
+	readNode, err := storage.ReadNode(node.ID())
+	assert.NoError(t, err)
+	assert.Equal(t, readNode.Title(), testTitle)
+	assert.Equal(t, readNode.Content(), testDescription)
+	assert.Equal(t, readNode.Slug(), testSlug)
+
+	// now, delete the node
+	err = storage.DeleteNode(node.ID())
+	assert.NoError(t, err)
+
+	// finally, verify that the node has been deleted
+	_, err = storage.ReadNode(node.ID())
+	assert.Error(t, err)
+}
+
 func TestFileStorage_CreateNode(t *testing.T) {
 	tempDir := t.TempDir()
 	storage := storage.NewFileStorage(tempDir)
@@ -96,4 +121,16 @@ func TestFileStorage_UpdateNode(t *testing.T) {
 	// check that actual file exists
 	_, err := os.Stat(storage.GetNodeFilePath(node.ID()))
 	assert.NoError(t, err)
+}
+
+func TestFileStorage_DeleteNode(t *testing.T) {
+	tempDir := t.TempDir()
+	storage := storage.NewFileStorage(tempDir)
+
+	node := models.NewSpec("Test Spec", "This should be deleted.")
+	GenericStorage_TestDeleteNode(t, storage, node)
+
+	// check that actual file no longer exists
+	_, err := os.Stat(storage.GetNodeFilePath(node.ID()))
+	assert.Error(t, err)
 }
